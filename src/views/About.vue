01d969e70d50526e1631b6e657642b709e817329
Beaumont Yun <b.yun@hotmail.com>
2021-08-30 13:54:12 +0800

    Minor update to spacing and typo

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,8 @@
   <div class="container md:w-8/12 mb-20 md:mr-80">
     <p class="font-semibold md:text-4xl">Got some stuff on your chest?</p>
     <br>
-    <p class="text-xl md:text-4xl">Could be a hair, could be ketchup, or it could be your next idea - 
-      comes <a href="https://calendly.com/b-yun/say-hi-to-beaumont" class="text-blue-450">say hi</a> and don't be shy, I always have a cup of coffee in hand to hear your thoughts</p>
+    <p class="text-xl md:text-4xl">Could be a hair, could be ketchup, or it could be your next great idea - 
+      come <a href="https://calendly.com/b-yun/say-hi-to-beaumont" class="text-blue-450">say hi</a> and don't be shy, I always have a cup of coffee in hand to hear your thoughts</p>
     <br>
     <p class="text-xl md:text-4xl">Some topics we could discuss:</p>
     <ul class="md:text-3xl">
@@ -15,7 +15,7 @@
       <li>- THE WORLD!</li>
     </ul>
     <br>
-    <p class="text-xl md:text-4xl">But just in case feellike conversing the old fashion way, you can always <a href="mailto:b.yun@hotmail.com" class="text-blue-450">email me</a></p>
+    <p class="text-xl md:text-4xl">But just in case you feel like conversing the old fashion way, you can always <a href="mailto:b.yun@hotmail.com" class="text-blue-450">email me</a></p>
     <br>
   </div>
 </template>
